3fd05c7a07d6430fda0f12c6635269acaae5eabd
royweb <xytank@gmail.com>
2020-11-23 12:22:51 -0500

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-center">File Storage In Blockchain</h1>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark p-0 text-monospace">
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="/"
+        >
+            FSIBC
+        </a>
+        <ul className="navbar-nav px-3">
+          <b className='text-white'>{'0x0'}</b>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
